0f23ec7eb8e65ca1dcdb1b9a7b4c984e49f7bdf2
Polina Rybachuk <polia6v@gmail.com>
2025-05-19 23:22:11 +0200

    19.05 23:22

--- NET/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ NET/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83e7a841fa5f8c05138b08dd7aea3f7e17062531")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b98e35cb583cfe979c8e1580f2033011bf3358ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
